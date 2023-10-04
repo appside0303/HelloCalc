e78b2610d0253ba3270eed0d12e93f3ede09256e
appside0303 <nexus6user2023132432@gmail.com>
2023-10-04 14:46:02 +0900

    first commit

---
+++ HelloCalc/Module1.vb
@@ -0,0 +1,25 @@
+﻿'
+' VB.netからcalcを呼び出す例題
+'
+Module Module1
+
+    Sub Main()
+        Dim factory As Object
+        Dim loader As Object
+        Dim component As Object
+        Dim args(0) As Object
+
+        factory = CreateObject("com.sun.star.ServiceManager")
+        loader = factory.createInstance("com.sun.star.frame.Desktop")
+        component = loader.loadComponentFromURL("private:factory/scalc", "_blank", 0, args)
+
+        Dim sheet As Object
+        Dim cell As Object
+
+        sheet = component.getSheets().getByName("Sheet1")
+        cell = sheet.getCellByPosition(1, 1)
+        cell.setFormula("hello, calc.")
+    End Sub
+
+End Module
+
